e363bec971b7be59172a4c9cc3c334cdb0e55945
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2024-06-27 12:10:14 -0300

    feat: add projects

---
+++ any-api/src/main/java/com/rodrigues/silva/marcos/ms/any_api/AnyApiApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigues.silva.marcos.ms.any_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnyApiApplication.class, args);
+	}
+
+}
---
+++ any-api/src/test/java/com/rodrigues/silva/marcos/ms/stateful_any_api/AnyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigues.silva.marcos.ms.stateful_any_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
