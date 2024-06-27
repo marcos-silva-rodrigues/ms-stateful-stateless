bde9217e19eef2198b674bb8695dea74c72a5d43
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2024-06-27 15:59:12 -0300

    feat: add exception and global handler

---
+++ any-api/src/main/java/com/rodrigues/silva/marcos/ms/any_api/infra/exception/ExceptionGlobalHandler.java
@@ -0,0 +1,28 @@
+package com.rodrigues.silva.marcos.ms.any_api.infra.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
+
+@RestControllerAdvice
+public class ExceptionGlobalHandler {
+
+  @ExceptionHandler(ValidationException.class)
+  public ProblemDetail handle(ValidationException ex) {
+    var problem = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
+    problem.setTitle("Validation Error");
+    problem.setProperty("timestamp", Instant.now());
+    return problem;
+  }
+
+  @ExceptionHandler(AuthenticationException.class)
+  public ProblemDetail handle(AuthenticationException ex) {
+    var problem = ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, ex.getMessage());
+    problem.setTitle("Validation Error");
+    problem.setProperty("timestamp", Instant.now());
+    return problem;
+  }
+}
---
+++ auth-api/src/main/java/com/rodrigues/silva/marcos/ms/auth_api/infra/exception/ExceptionGlobalHandler.java
@@ -0,0 +1,31 @@
+package com.rodrigues.silva.marcos.ms.auth_api.infra.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.net.URI;
+import java.time.Instant;
+
+@RestControllerAdvice
+public class ExceptionGlobalHandler {
+
+  @ExceptionHandler(ValidationException.class)
+  public ProblemDetail handle(ValidationException ex) {
+    var problem = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
+    problem.setTitle("Validation Error");
+    problem.setProperty("timestamp", Instant.now());
+    return problem;
+  }
+
+  @ExceptionHandler(AuthenticationException.class)
+  public ProblemDetail handle(AuthenticationException ex) {
+    var problem = ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, ex.getMessage());
+    problem.setTitle("Validation Error");
+    problem.setProperty("timestamp", Instant.now());
+    return problem;
+  }
+}
