711560c86aaaea10cd5b653b86c7c7e3bdd1de15
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2024-06-28 12:05:42 -0300

    feat: auth controller

---
+++ auth-api/src/main/java/com/rodrigues/silva/marcos/ms/auth_api/core/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.rodrigues.silva.marcos.ms.auth_api.core.controller;
+
+import com.rodrigues.silva.marcos.ms.auth_api.core.dto.AuthRequest;
+import com.rodrigues.silva.marcos.ms.auth_api.core.dto.TokenDTO;
+import com.rodrigues.silva.marcos.ms.auth_api.core.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/auth")
+@AllArgsConstructor
+public class AuthController {
+
+  private final AuthService authService;
+
+  @PostMapping("login")
+  public TokenDTO login(@RequestBody AuthRequest authRequest) {
+    return authService.login(authRequest);
+  }
+
+  @PostMapping("validate")
+  public TokenDTO validate(@RequestHeader("token") String accessToken) {
+    return authService.validateToken(accessToken);
+  }
+}
